103152d67e8f6edba809838e193b72f0851fd640
Navy <navydotgif@gmail.com>
2020-02-20 22:48:55 +0200

    client

---
+++ Structures/Client.js
@@ -0,0 +1,38 @@
+const Registry = require('./Registry.js');
+
+const Express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+class Client {
+
+    constructor(index, options = {}) {
+
+        if(!index) throw new Error('Missing index!');
+        this.index = index;
+        this.options = options;
+        this.logger = index.logger;
+        this.database = index.database;
+
+        this.registry = new Registry(this);
+
+        this.app = Express();
+        this.app.listen(3000, (err) => { if(err) this.logger.warn(err); else this.logger.print(`Listening on port 3000`) });
+
+        //Set up rendering engine
+        this.app.engine('.hbs', exphbs({
+            defaultLayout: 'main',
+            extname: '.hbs',
+            layoutsDir: path.join(__dirname, 'views/layouts')
+        }));
+        this.app.set('view engine', '.hbs');
+        this.app.set('views', path.join(__dirname, 'views'));
+        
+        //Registry has to be finalized after express has been set up
+        this.registry.init().then(() => console.log('registry done'));
+
+    }
+
+}
+
+module.exports = Client;
