c5d1e86c9205c13c4135681edd51b5a9d4818678
Navy <navydotgif@gmail.com>
2020-02-20 22:48:14 +0200

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     debug: true,
-    started: ~~(Date.now()/1000)
+    started: ~~(Date.now()/1000),
+    dir: process.cwd()
 }
 
 require('./Utilities/Startup.js').init(module.exports);
