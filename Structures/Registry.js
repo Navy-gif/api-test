6664d424bc36d2a5650bbf2074200bfc099f5f62
Erik <navydotgif@gmail.com>
2020-02-22 22:19:10 +0200

    recursive folder reading & print getter

--- Structures/Registry.js
+++ Structures/Registry.js
@@ -10,22 +10,39 @@ class Registry {
 
     }
 
+    get print() {
+
+        let out = [];
+        this.endpoints.forEach(ep => out.push(ep.name));
+        return 'Registry print: ' + out.join(', ');
+
+    }
+
     async init() {
 
         let dir_path = path.join(this.client.index.dir, 'Endpoints');
-        let endpoints_dir = fs.readdirSync(dir_path);
+        let self = this;
+
+        (function read(pth) {
+
+            let endpoints_dir = fs.readdirSync(pth, { withFileTypes: true });
+
+            for(let file of endpoints_dir) {
+
+                if(file.isDirectory()) read(path.join(pth, file.name));
+                else {
+                    self.client.logger.print(`Loading endpoint ${file.name}`);
+                    file = path.join(pth, file.name);
+                    let endpoint = require(file);
+                    if(!endpoint) self.client.logger.warn(`Issue requiring \`${file}\``);
         
-        for(let file of endpoints_dir) {
+                    endpoint = new endpoint(self.client);
+                    self.endpoints.set(endpoint.name, endpoint);
+                }
+    
+            }
 
-            this.client.logger.print(`Loading endpoint ${file}`);
-            file = path.join(dir_path, file);
-            let endpoint = require(file);
-            if(!endpoint) this.client.logger.warn(`Issue requiring \`${file}\``);
-
-            endpoint = new endpoint(this.client);
-            this.endpoints.set(endpoint.name, endpoint);
-
-        }
+        })(dir_path);
 
     }
 
