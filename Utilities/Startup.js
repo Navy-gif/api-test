ac221295e9bbd49470e82afe697262e527931c0c
Erik <navydotgif@gmail.com>
2020-02-22 22:19:21 +0200

    proper error logging

--- Utilities/Startup.js
+++ Utilities/Startup.js
@@ -16,19 +16,37 @@ class Startup {
         index.logger.error('Error test.');
         index.logger.warn('Warning test.');
 
-        process.on('uncaughtException', e => index.logger.error(e));
-        process.on('unhandledRejection', e => index.logger.error(e));
-        process.on('warning', e => index.logger.warn(e));
+        process.on('uncaughtException', e => {
+            index.logger.error('Uncaught exception:');
+            index.logger.error(e.stack);
+        });
+        process.on('unhandledRejection', e => {
+            index.logger.error('Unhandled rejection:');
+            index.logger.error(e.stack);
+        });
+        process.on('warning', e => {
+            index.logger.warn('Warning:');
+            index.logger.warn(e.stack);
+        });
 
         //Set up database connection
         let Database = require('./Database.js');
-        index.database = await new Database(index, require('../Config/Mongodb.json')).init();
+        //index.database = await new Database(index, require('../Config/Mongodb.json')).init();
 
         //Initiate client
-        let Client = require('../Structures/Client.js');
+        let key, cert, Client = require('../Structures/Client.js');
+
+        try {
+            key = fs.readFileSync(path.join(index.dir, 'host.key.pem'));
+            cert = fs.readFileSync(path.join(index.dir, 'host.crt.pem'));
+        } catch(err) {
+            index.logger.error('Could not find key or certificate!');
+            index.logger.error(err.stack);
+        }
+
         index.client = new Client(index, {
-            key: fs.readFileSync(path.join(index.dir, 'host.key.pem')),
-            cert: fs.readFileSync(path.join(index.dir, 'host.crt.pem'))
+            key: key,
+            cert: cert
         });
 
     }
