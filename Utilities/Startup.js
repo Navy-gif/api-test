bb66cb01a5a46efeaf000cbf3c25bd54ec853df3
Erik <navydotgif@gmail.com>
2020-02-22 19:12:44 +0200

    error logging

--- Utilities/Startup.js
+++ Utilities/Startup.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 class Startup {
 
     static async init(index) {
@@ -13,13 +16,20 @@ class Startup {
         index.logger.error('Error test.');
         index.logger.warn('Warning test.');
 
+        process.on('uncaughtException', e => index.logger.error(e));
+        process.on('unhandledRejection', e => index.logger.error(e));
+        process.on('warning', e => index.logger.warn(e));
+
         //Set up database connection
         let Database = require('./Database.js');
         index.database = await new Database(index, require('../Config/Mongodb.json')).init();
 
         //Initiate client
         let Client = require('../Structures/Client.js');
-        index.client = new Client(index);
+        index.client = new Client(index, {
+            key: fs.readFileSync(path.join(index.dir, 'host.key.pem')),
+            cert: fs.readFileSync(path.join(index.dir, 'host.crt.pem'))
+        });
 
     }
 
